245d2fbdfc9b84c42978f1fe32b0858669cceeeb
Jon2G <jonyjouna@gmail.com>
2021-11-11 17:07:50 -0600

    Elastic beanstalk

--- StorageProvider/WStorageProvider/Startup.cs
+++ StorageProvider/WStorageProvider/Startup.cs
@@ -38,6 +38,7 @@ namespace WStorageProvider
                     var clientConfig = new AmazonDynamoDBConfig { ServiceURL = dynamoDbConfig.GetValue<string>("LocalServiceUrl") };
                     return new AmazonDynamoDBClient(clientConfig);
                 });
+                services.AddSwaggerGen();
             }
             else
             {
@@ -54,6 +55,13 @@ namespace WStorageProvider
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
+
             }
 
             app.UseHttpsRedirection();
